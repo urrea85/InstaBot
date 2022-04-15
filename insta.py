e33054f5453de714d1cd55513b0b935af455ea8d
urrea <urrea@rocky.com>
2022-04-15 15:18:35 +0200

    Colors and display password

--- insta.py
+++ insta.py
@@ -3,11 +3,18 @@ from selenium import webdriver
 from getpass4 import getpass
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+import colorama
+from colorama import Fore
 import pyfiglet
+from colored import fg
+from colorama import Style
 
 browser = webdriver.Chrome()
 browser.get("https://www.instagram.com/")
 sleep(2)
+
+error = fg('red')
+alert = fg('yellow')
 
 def login(username, password):
 	username_input = browser.find_element(By.CSS_SELECTOR,"input[name='username']")
@@ -31,27 +38,41 @@ def accept_cookies():
 		browser.find_element(By.XPATH,"//button[text()='Permitir solo cookies necesarias']").click()
 
 def open_followers(account):
-	browser.get("https://www.instagram.com/"+ account + "/followers/")
-	sleep(3)
-	browser.find_element(By.XPATH,"//div[text()=' seguidores']").click()
+	try:
+		browser.get("https://www.instagram.com/"+ account + "/followers/")
+		sleep(3)
+		browser.find_element(By.XPATH,"//div[text()=' seguidores']").click()
+		return True
+	except:
+		print(error + "Error: Invalid username" + Style.RESET_ALL)
+		return False
 
 def scroll_followers(minutes):
-	pop_up = browser.find_element(By.XPATH,"//div[@class='isgrP']")
-	timeout = time() + 60 * minutes
-	while True:
-		if time() > timeout:
-			break
-		browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',pop_up)
-		sleep(1)
+	try:
+		pop_up = browser.find_element(By.XPATH,"//div[@class='isgrP']")
+		timeout = time() + 60 * minutes
+		while True:
+			if time() > timeout:
+				break
+			browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;',pop_up)
+			sleep(1)
+
+		return True
+	except:
+		print(error + "Error: Try it again" + Style.RESET_ALL)
+		return False
 
 def follow_followers():
-	list_followers = browser.find_element(By.XPATH,"//div[@class='PZuss']")
-	for child in list_followers.find_element(By.CSS_SELECTOR,'li'):
-		follow = child.find.find_element(By.CSS_SELECTOR,"button")
-		if follow.text == "Seguir":
-			follow.click()
-		else:
-			pass #already followed
+	try:
+		list_followers = browser.find_element(By.XPATH,"//div[@class='PZuss']")
+		for child in list_followers.find_element(By.CSS_SELECTOR,'li'):
+			follow = child.find.find_element(By.CSS_SELECTOR,"button")
+			if follow.text == "Seguir":
+				follow.click()
+			else:
+				pass #already followed
+	except:
+		print(error + "Error: Try it again" + Style.RESET_ALL)
 
 accept_cookies()
 
@@ -61,23 +82,31 @@ ascii_banner = pyfiglet.figlet_format("InstaBot")
 print(ascii_banner)
 print("By urrea")
 while opt:
-	print(f"1. LogIn")
+	print (" ")
+	print(f"1. Log In")
 	print(f"2. Follow all followers of a user")
-	print(f"1. Exit")
-	options = input("Select option [1-3]:")
+	print(f"3. Mentions in a publication")
+	print(f"4. Common friends between 2 users")
+	print(f"5. Exit")
+	options = input("Select option [1-5]:")
+
 	if options == '1':
 		user = input("Username: ")
-		passwd = input("Password: ")
+		passwd = getpass("Password: ")
 		login(user,passwd)
 	elif options == '2':
 		fol = input("Select the user: ")
-		open_followers(fol)
-		sleep(3)
-		scroll_followers(2)
-		#follow_followers()
-	elif options == '3':
+		if open_followers(fol):
+			sleep(3)
+			if scroll_followers(2):
+				print(f"Following all users...")
+				#follow_followers()
+	elif options == '5':
 		print(f"Bye...")
 		browser.quit()
 		opt = False
 	else:
 		print(f'Select a valid option')
+
+	print(" ")
+	print(alert + "Alert: Don't touch the web, the bot will not work!" + Style.RESET_ALL)
